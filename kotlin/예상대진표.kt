c9805ca88c03be300733bd52e82942ca9b276fd8
woojin7879 <woojin7879@unist.ac.kr>
2022-08-10 03:23:41 +0900

    220809

--- kotlin/예상대진표.kt
+++ kotlin/예상대진표.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun solution(n: Int, a: Int, b: Int): Int {
-        var answer = 0
+        var answer = 0;
         var numa = a;
         var numb = b;
         for (i in 1 until 22) {
             numa = (numa + 1) / 2;
             numb = (numb + 1) / 2;
             if (numa ==  numb) {
-                answer = i
+                answer = i;
                 break;
             }
         }
